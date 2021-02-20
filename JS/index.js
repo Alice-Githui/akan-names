4fce308bc9c633f07018516b0f803f16aee74290
Alice-Githui <gnyokabi.alice@gmail.com>
2021-02-20 11:47:28 +0300

    Add the table containing the names assigned to kids depending on gender

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+/*
+Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+ where;
+
+ CC - is the century digits. For example 1989 has CC = 19
+
+ YY - is the Year digits (1989 has YY = 89)
+
+ MM -  is the Month
+
+ DD - is the Day of the month 
+
+ mod - is the modulus function ( % )*/
+ 
+ /*day =(((CC/4) - 2*CC -1) + ((5*YY/4)) + ((26*(MM+1/10)) + DD) mod 7 */
+
+ var cc = (document.getElementById("Id1").value);
+ var yy = (document.getElementById("Id2").value);
+ var mm = (document.getElementById("Id3").value);
+ var dd = (document.getElementById("Id4").value);
+ var d = (((cc/4) - 2*cc-1) + ((5*yy/4)) + ((26*(mm + 1) /10)) + dd ) % 7 //derived from the formulae//
+
+ function day(){
+     if (d = )
+
+ }
