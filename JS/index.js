69c945455b0e367bc8ab4df17296d8e68e8c04c1
Alice-Githui <gnyokabi.alice@gmail.com>
2021-02-20 15:05:22 +0300

    Add the conditional functions to the JS

--- JS/index.js
+++ JS/index.js
@@ -15,8 +15,6 @@ Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD 
  
  /*day =(((CC/4) - 2*CC -1) + ((5*YY/4)) + ((26*(MM+1/10)) + DD) mod 7 */
 
- var daysArray= ["[Sunday]" , "[Monday]" , "[Tuesday]" , "[Wednesday]" , "[Thusday]" , "[Friday]" , "[Saturday]" ]
-
 
  function getDay (){
     var cc = (document.getElementById("Id1").value);
@@ -48,4 +46,53 @@ Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD 
       Friday = "Koh"
       Saturday = "Kwame"
    }
- 
+function calcDay{
+   //Enter Elements//
+   var day = null,
+    var cc = (document.getElementById("Id1").value);
+    var yy = (document.getElementById("Id2").value);
+    var mm = (document.getElementById("Id3").value);
+    var dd = (document.getElementById("Id4").value);
+    results= (document.getElementById("dayoftheweek"))
+
+   day = (((cc/4) - 2*cc-1) + ((5*yy/4)) + ((26*(mm + 1) /10)) + dd ) % 7
+
+   if (female && Sunday){
+      results = "Akosua"
+   } else if (male && Sunday) {
+      results = "Kwasi"
+   } 
+   if (Female && Monday){
+      results = "Adwoa"
+   } else if (male && Monday){
+      results = "Kwadwo"
+   }
+
+   if (Female && Tuesday){
+      results = "Abenaa"
+   } else if (male && Tuesday){
+      results = "Kwabena"
+   }
+
+   if(Female && Wednesday){
+      results ="Akua"
+   } else if (Male && Wednesday){
+      results = "Kwaku"
+   }
+
+   if (Female && Thursday){
+      results= "Yaa"
+   } else if (Male && Thursday){
+      results ="Yaw"
+   }
+
+   if(Female && Friday){
+      results = "Afua"
+   } else if (Male && Friday){
+      results ="Koh"
+   }
+   if(Female && Saturday){
+      results = "Ama"
+   } else if (Male && Saturday) {
+      results = "Kwame"
+   }
