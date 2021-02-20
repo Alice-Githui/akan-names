b0a0a3c011fe93fff1bff932bd0a329371e12726
Alice-Githui <gnyokabi.alice@gmail.com>
2021-02-20 14:06:41 +0300

    Add the id's to the birthdates

--- JS/index.js
+++ JS/index.js
@@ -15,13 +15,24 @@ Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD 
  
  /*day =(((CC/4) - 2*CC -1) + ((5*YY/4)) + ((26*(MM+1/10)) + DD) mod 7 */
 
- var cc = (document.getElementById("Id1").value);
- var yy = (document.getElementById("Id2").value);
- var mm = (document.getElementById("Id3").value);
- var dd = (document.getElementById("Id4").value);
- var d = (((cc/4) - 2*cc-1) + ((5*yy/4)) + ((26*(mm + 1) /10)) + dd ) % 7 //derived from the formulae//
+ var daysArray= ["[Sunday]" , "[Monday]" , "[Tuesday]" , "[Wednesday]" , "[Thusday]" , "[Friday]" , "[Saturday]" ]
 
- function day(){
-     if (d = )
+
+ function getDay (){
+    var cc = (document.getElementById("Id1").value);
+    var yy = (document.getElementById("Id2").value);
+    var mm = (document.getElementById("Id3").value);
+    var dd = (document.getElementById("Id4").value);
+    var d = (((cc/4) - 2*cc-1) + ((5*yy/4)) + ((26*(mm + 1) /10)) + dd ) % 7 //derived from the formulae// */
+
+    var todayDate = new Date ()
+    var today_day = today_day.getDate ();
+    var today_month = today_day.getMonth ();
+    var today_year = today_day.getYear ();
+
+    var day = (((cc/4) - 2*cc-1) + ((5*yy/4)) + ((26*(mm + 1) /10)) + dd ) % 7
+
+    if 
 
  }
+ 
