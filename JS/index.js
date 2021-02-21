ee37fc58aae5f4429fc32b8802f25fac9aa44418
Alice-Githui <gnyokabi.alice@gmail.com>
2021-02-21 19:57:14 +0300

    Add the day picker to index.js

--- JS/index.js
+++ JS/index.js
@@ -9,53 +9,51 @@ var female = ['Akosua' , 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
     let male =(document.getElementById("Male").value)
     let female =(document.getElementById("Female").value)
 
-//Day picker
-let fdate = new Date(date).toDateString();
- var mdate = fdate.split('')[0];
-document.getElementById("result").innerHTML = mdate;
+    let fdate = new Date(date).toDateString();
+    let mdate = fdate.split('')[0];
+    document.getElementById("result").innerHTML = mdate;
 
-if(mdate == day[0] && male){
-   console.log(male[0]);
-} else if (mdate == day[1] && male){
-   console.log(male[1])
-} else if (mdate == day[2] && male){
-   console.log(male[2])
-} else if (mdate == day[3] && male){
-   console.log(male[3])
-} else if (mdate == day[4] && male){
-   console.log(male[4])
-} else if (mdate == day[5] && male){
-   console.log(male[5])
-} else if (mdate == day[6] && male){
-   console.log(male[6])
-} else if (mdate == day[7] && male){
-   console.log(male[7])
-}
+    if(mdate == day[0] && male){
+      console.log(male[0]);
+   } else if (mdate == day[1] && male){
+      console.log(male[1])
+   } else if (mdate == day[2] && male){
+      console.log(male[2])
+   } else if (mdate == day[3] && male){
+      console.log(male[3])
+   } else if (mdate == day[4] && male){
+      console.log(male[4])
+   } else if (mdate == day[5] && male){
+      console.log(male[5])
+   } else if (mdate == day[6] && male){
+      console.log(male[6])
+   } else if (mdate == day[7] && male){
+      console.log(male[7])
+   }
+   
+   }
+      
+   //control flow for female 
+   
+   if (mdate == day[0] && female){
+      console.log(female[0]);
+   } else if (mdate == day[1] && female){
+      console.log(female[1])
+   } else if (mdate == day[2] && female){
+      console.log(female[2])
+   } else if (mdate == day[3] && female){
+      console.log(female[3])
+   } else if (mdate == day[4] && female){
+      console.log(female[4])
+   } else if (mdate == day[5] && female){
+      console.log(female[5])
+   } else if (mdate == day[6] && female){
+      console.log(female[6])
+   } else if (mdate == day[7] && female){
+      console.log(female[7])
+   } 
 
-}
 
-/*//Day picker
-let fdate = new Date(date).toDateString();
- var mdate = fdate.split('')[0];
-document.getElementById("result").innerHTML = mdate;*/
 
-//control flow for female 
 
-if (mdate == day[0] && female){
-   console.log(female[0]);
-} else if (mdate == day[1] && female){
-   console.log(female[1])
-} else if (mdate == day[2] && female){
-   console.log(female[2])
-} else if (mdate == day[3] && female){
-   console.log(female[3])
-} else if (mdate == day[4] && female){
-   console.log(female[4])
-} else if (mdate == day[5] && female){
-   console.log(female[5])
-} else if (mdate == day[6] && female){
-   console.log(female[6])
-} else if (mdate == day[7] && female){
-   console.log(female[7])
-} 
 
