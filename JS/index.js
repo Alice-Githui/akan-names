c55eb9a7ce9367ebfdc14427c8127bb2c3648035
Alice-Githui <gnyokabi.alice@gmail.com>
2021-02-21 21:31:04 +0300

    Add the variables date and day

--- JS/index.js
+++ JS/index.js
@@ -6,28 +6,28 @@ var female = ['Akosua' , 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
  function getData(){
     event.preventDefault();
     var date = (document.getElementById('date').value)
-    let male =(document.getElementById('Male').value)
-    let female =(document.getElementById('Female').value)
+    var male =(document.getElementById('Male').value)
+    var female =(document.getElementById('Female').value)
 
-    let fdate = new Date(date).toDateString();
-    var day = fdate.split('')[0];
-    document.getElementById('result').innerHTML = day;
+    var adate = new Date(date).toDateString();
+    var aday = adate.split('')[0];
+    document.getElementById('result').innerHTML = aday;
 
-    if(day == day[0] && male){
+    if(aday == day[0] && male){
       console.log(male[0]);
-   } else if (day == day[1] && male){
+   } else if (aday == day[1] && male){
       console.log(male[1])
-   } else if (day == day[2] && male){
+   } else if (aday == day[2] && male){
       console.log(male[2])
-   } else if (day == day[3] && male){
+   } else if (aday == day[3] && male){
       console.log(male[3])
-   } else if (day == day[4] && male){
+   } else if (aday == day[4] && male){
       console.log(male[4])
-   } else if (day == day[5] && male){
+   } else if (aday == day[5] && male){
       console.log(male[5])
-   } else if (day == day[6] && male){
+   } else if (aday == day[6] && male){
       console.log(male[6])
-   } else if (day == day[7] && male){
+   } else if (aday == day[7] && male){
       console.log(male[7])
    }
    
@@ -35,21 +35,21 @@ var female = ['Akosua' , 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
       
    //control flow for female 
    
-   if (day == day[0] && female){
+   if (aday == day[0] && female){
       console.log(female[0]);
-   } else if (day == day[1] && female){
+   } else if (aday == day[1] && female){
       console.log(female[1])
-   } else if (day == day[2] && female){
+   } else if (aday == day[2] && female){
       console.log(female[2])
-   } else if (day == day[3] && female){
+   } else if (aday == day[3] && female){
       console.log(female[3])
-   } else if (day == day[4] && female){
+   } else if (aday == day[4] && female){
       console.log(female[4])
-   } else if (day == day[5] && female){
+   } else if (aday == day[5] && female){
       console.log(female[5])
-   } else if (day == day[6] && female){
+   } else if (aday == day[6] && female){
       console.log(female[6])
-   } else if (day == day[7] && female){
+   } else if (aday == day[7] && female){
       console.log(female[7])
    } 
 
